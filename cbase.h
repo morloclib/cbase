66046d37135b54542152e9070c3544568c2b2459
Zebulun Arendsee <arendsee@iastate.edu>
2019-06-10 13:21:38 -0500

    Add int serialization

--- cbase.h
+++ cbase.h
@@ -4,14 +4,27 @@
 char* packDouble(double x);
 double unpackDouble(char* json);
 
+char* packInt(int x);
+int unpackInt(char* json);
+
 char* packDouble(double x){
   char* x_str = (char*)malloc(50 * sizeof(char));
-  sprintf(x_str, "%g", x);
+  sprintf(x_str, "%lf", x);
+  return x_str;
+}
+char* packInt(int x){
+  char* x_str = (char*)malloc(50 * sizeof(char));
+  sprintf(x_str, "%ld", x);
   return x_str;
 }
 
 double unpackDouble(char* json){
   double x;
   sscanf(json, "%lf", &x);
+  return x;
+}
+int unpackInt(char* json){
+  int x;
+  sscanf(json, "%ld", &x);
   return x;
 }
