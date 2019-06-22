7d514c3dc92cbe6ee4d7c1cba62a22b944b1e2dd
Zebulun Arendsee <arendsee@iastate.edu>
2019-06-21 21:44:02 -0500

    Add string and json c types

--- cbase.h
+++ cbase.h
@@ -7,6 +7,9 @@ double unpackDouble(char* json);
 char* packInt(int x);
 int unpackInt(char* json);
 
+char* packString(char* x);
+char* unpackString(char* x);
+
 char* packDouble(double x){
   char* x_str = (char*)malloc(50 * sizeof(char));
   sprintf(x_str, "%lf", x);
@@ -16,6 +19,9 @@ char* packInt(int x){
   char* x_str = (char*)malloc(50 * sizeof(char));
   sprintf(x_str, "%ld", x);
   return x_str;
+}
+char* packString(int x){
+  return x;
 }
 
 double unpackDouble(char* json){
@@ -27,4 +33,7 @@ int unpackInt(char* json){
   int x;
   sscanf(json, "%ld", &x);
   return x;
+}
+int unpackString(char* json){
+  return json;
 }
