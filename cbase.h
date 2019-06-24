1d490f1383bd465cf051c158e5ed59d2a9fb54d5
Zebulun Arendsee <arendsee@iastate.edu>
2019-06-24 09:13:48 -0500

    Add untested 'foreign_call' function

--- cbase.h
+++ cbase.h
@@ -1,5 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
+#define BUFFER_SIZE 1024
+
+char* foreign_call(char* cmd);
 
 char* packDouble(double x);
 double unpackDouble(char* json);
@@ -9,6 +14,35 @@ int unpackInt(char* json);
 
 char* packString(char* x);
 char* unpackString(char* x);
+
+char* foreign_call(char* cmd)
+{
+    FILE *fh = popen(cmd, "r");
+    size_t nbytes = sizeof(char) * BUFFER_SIZE;
+    size_t i = 1;
+    size_t offset = 0;
+    char* json;
+    char* old_json;
+    char* buffer = calloc(BUFFER_SIZE, sizeof(char));
+    while (fgets(buffer, BUFFER_SIZE, fh) !=NULL){
+        if(i == 1){
+            json = malloc(nbytes);
+            memcpy(json, buffer, nbytes);
+        } else {
+            // TODO: make this more memory efficient
+            old_json = json;
+            offset = (i-1) * nbytes;
+            json = malloc(i * nbytes);
+            memcpy(json, old_json, offset);
+            memcpy(json + offset + 1, buffer, nbytes);
+            free(old_json);
+        }
+        i++;
+    }
+    pclose(fh);
+    free(buffer);
+    return json;
+}
 
 char* packDouble(double x){
   char* x_str = (char*)malloc(50 * sizeof(char));
